92a8212055e0b798fc67ec6e1bd208dcb32d2c15
conorbroderick <cjayjayb@gmail.com>
2017-07-07 16:00:59 +0100

    Expose loaded blackbox configuration on /config

--- config_test.go
+++ config_test.go
@@ -1,6 +1,11 @@
 package main
 
-import "testing"
+import (
+	"strings"
+	"testing"
+
+	yaml "gopkg.in/yaml.v2"
+)
 
 func TestLoadConfig(t *testing.T) {
 	sc := &SafeConfig{
@@ -23,5 +28,28 @@ func TestLoadBadConfig(t *testing.T) {
 	err := sc.reloadConfig("testdata/blackbox-bad.yml")
 	if err.Error() != expected {
 		t.Errorf("\nexpected:\n%v\ngot:\n%v", expected, err.Error())
+	}
+}
+
+func TestHideConfigSecrets(t *testing.T) {
+
+	sc := &SafeConfig{
+		C: &Config{},
+	}
+
+	err := sc.reloadConfig("testdata/blackbox-good.yml")
+	if err != nil {
+		t.Errorf("Error loading config %v: %v", "testdata/blackbox-good.yml", err)
+	}
+
+	// String method must not reveal authentication credentials.
+	sc.RLock()
+	c, err := yaml.Marshal(sc.C)
+	sc.RUnlock()
+	if err != nil {
+		t.Errorf("Error marshalling config: %v", err)
+	}
+	if strings.Contains(string(c), "mysecret") {
+		t.Fatal("config's String method reveals authentication credentials.")
 	}
 }
