7840f7855f75d7159686aa3f38ab0f5bcb502859
Conor Broderick <conor.broderick@robustperception.io>
2017-07-11 17:14:07 +0100

    Expose HTTP version of probe (#189)

--- config.go
+++ config.go
@@ -36,6 +36,7 @@ type Module struct {
 type HTTPProbe struct {
 	// Defaults to 2xx.
 	ValidStatusCodes       []int                   `yaml:"valid_status_codes,omitempty"`
+	ValidHTTPVersions      []string                `yaml:"valid_http_versions,omitempty"`
 	PreferredIPProtocol    string                  `yaml:"preferred_ip_protocol,omitempty"`
 	NoFollowRedirects      bool                    `yaml:"no_follow_redirects,omitempty"`
 	FailIfSSL              bool                    `yaml:"fail_if_ssl,omitempty"`
