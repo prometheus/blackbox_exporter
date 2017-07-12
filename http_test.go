582bba5f94fe113051c318ba8c6758cef5b10372
Conor Broderick <conor.broderick@robustperception.io>
2017-07-12 17:09:59 +0100

    Added test for tcp timeout (#192)

--- http_test.go
+++ http_test.go
@@ -483,3 +483,37 @@ func TestTLSConfigIsIgnoredForPlainHTTP(t *testing.T) {
 	}
 	checkRegistryResults(expectedResults, mfs, t)
 }
+
+var c = &Config{
+	Modules: map[string]Module{
+		"http_2xx": Module{
+			Prober:  "http",
+			Timeout: 10 * time.Second,
+		},
+	},
+}
+
+func TestPrometheusTimeoutHTTP(t *testing.T) {
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(2 * time.Second)
+	}))
+	defer ts.Close()
+
+	req, err := http.NewRequest("GET", "?target="+ts.URL, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("X-Prometheus-Scrape-Timeout-Seconds", "1")
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		probeHandler(w, r, c)
+	})
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("probe request handler returned wrong status code: %v, want %v", status, http.StatusOK)
+	}
+}
