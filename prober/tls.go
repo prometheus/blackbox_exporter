02d1b9aab1dd428fa181f04eb4a7ae08b6ca33eb
SilentGo <51835075+SilentGo@users.noreply.github.com>
2019-11-05 21:22:23 +0800

    exclude CA
    Check the certificate expiration date, exclude the CA authority, and get the last TLS expiration date by default. Sometimes it is the CA authority, and the expiration date of the certificate is not accurate

--- prober/tls.go
+++ prober/tls.go
@@ -21,7 +21,7 @@ import (
 func getEarliestCertExpiry(state *tls.ConnectionState) time.Time {
 	earliest := time.Time{}
 	for _, cert := range state.PeerCertificates {
-		if (earliest.IsZero() || cert.NotAfter.Before(earliest)) && !cert.NotAfter.IsZero() {
+		if (earliest.IsZero() || cert.NotAfter.Before(earliest)) && !cert.NotAfter.IsZero() && !cert.IsCA {
 			earliest = cert.NotAfter
 		}
 	}
