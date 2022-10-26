d6f683933f59f93d83efb6aa00e7251540e8d37e
Lyas Spiehler <lspiehler@gmail.com>
2022-10-26 20:10:48 +0000

    only add servername if TLS is true
    Signed-off-by: Lyas Spiehler <lspiehler@gmail.com>

--- prober/handler.go
+++ prober/handler.go
@@ -148,8 +148,10 @@ func Handler(w http.ResponseWriter, r *http.Request, c *config.Config, logger lo
 func setTLSServerName(hostname string, module *config.Module) error {
 	// By creating a new hashmap and copying values there we
 	// ensure that the initial configuration remain intact.
-	if module.TCP.TLSConfig.ServerName == "" {
-		module.TCP.TLSConfig.ServerName = hostname
+	if module.TCP.TLS {
+		if module.TCP.TLSConfig.ServerName == "" {
+			module.TCP.TLSConfig.ServerName = hostname
+		}
 	}
 	return nil
 }
