5400d9d88b9654ab4922b3cb18b7a17bda5c5838
Jan-Otto Kröpke <mail@jkroepke.de>
2023-07-17 20:58:37 +0200

    Add opt-in toggle
    Signed-off-by: Jan-Otto Kröpke <mail@jkroepke.de>

--- main.go
+++ main.go
@@ -50,6 +50,7 @@ var (
 	configFile    = kingpin.Flag("config.file", "Blackbox exporter configuration file.").Default("blackbox.yml").String()
 	timeoutOffset = kingpin.Flag("timeout-offset", "Offset to subtract from timeout in seconds.").Default("0.5").Float64()
 	configCheck   = kingpin.Flag("config.check", "If true validate the config file and then exit.").Default().Bool()
+	dynamicProbe  = kingpin.Flag("enable.dynamic-probe", "If true, probes can be dynamically configured via GET parameter using /probe/dynamic endpoint.").Default().Bool()
 	historyLimit  = kingpin.Flag("history.limit", "The maximum amount of items to keep in the history.").Default("100").Uint()
 	externalURL   = kingpin.Flag("web.external-url", "The URL under which Blackbox exporter is externally reachable (for example, if Blackbox exporter is served via a reverse proxy). Used for generating relative and absolute links back to Blackbox exporter itself. If the URL has a path portion, it will be used to prefix all HTTP endpoints served by Blackbox exporter. If omitted, relevant URL components will be derived automatically.").PlaceHolder("<url>").String()
 	routePrefix   = kingpin.Flag("web.route-prefix", "Prefix for the internal routes of web endpoints. Defaults to path of --web.external-url.").PlaceHolder("<path>").String()
@@ -186,9 +187,16 @@ func run() int {
 		sc.Unlock()
 		prober.Handler(w, r, conf, logger, rh, *timeoutOffset, nil, moduleUnknownCounter)
 	})
+
 	http.HandleFunc(path.Join(*routePrefix, "/probe/dynamic"), func(w http.ResponseWriter, r *http.Request) {
-		prober.DynamicHandler(w, r, logger, rh, *timeoutOffset, nil, moduleUnknownCounter)
+		if *dynamicProbe {
+			prober.DynamicHandler(w, r, logger, rh, *timeoutOffset, nil, moduleUnknownCounter)
+			return
+		}
+
+		w.WriteHeader(http.StatusNotFound)
 	})
+
 	http.HandleFunc(*routePrefix, func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/html")
 		w.Write([]byte(`<html>
