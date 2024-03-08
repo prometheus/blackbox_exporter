5c58a3801b438968cf286497a51734b9d9abd447
Shang Ding <rifflegrass@gmail.com>
2024-03-07 19:07:15 -0600

    tidy up helpProbeTLSCipher help text
    Signed-off-by: Shang Ding <rifflegrass@gmail.com>

--- prober/prober.go
+++ prober/prober.go
@@ -28,7 +28,7 @@ const (
 	helpSSLEarliestCertExpiry     = "Returns last SSL chain expiry in unixtime"
 	helpSSLChainExpiryInTimeStamp = "Returns last SSL chain expiry in timestamp"
 	helpProbeTLSInfo              = "Returns the TLS version used or NaN when unknown"
-	helpProbeTLSCipher            = "Returns the TLS cipher negotiated during handshake or 0x000 when unknown"
+	helpProbeTLSCipher            = "Returns the TLS cipher negotiated during handshake"
 )
 
 var (
