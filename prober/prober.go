ed6d767928da26432c041d2391af35bface5481c
Tobias Guggenmos <guggenmos@dfn-cert.de>
2024-12-06 12:28:02 +0100

    Basic Infrastructure for Handling probe failures
    Signed-off-by: Tobias Guggenmos <guggenmos@dfn-cert.de>

--- prober/prober.go
+++ prober/prober.go
@@ -22,13 +22,63 @@ import (
 	"github.com/prometheus/blackbox_exporter/config"
 )
 
-type ProbeFn func(ctx context.Context, target string, config config.Module, registry *prometheus.Registry, logger *slog.Logger) bool
+type ProbeResult struct {
+	success        bool
+	failureReason  string
+	failureDetails []string
+}
+
+// Creates the ProbeResult for a failed probe.
+//
+// Expects an odd number of string arguments.
+//
+// Example:
+// Calling probeFailure("problem", "label1", "value1", "label2", "value2")
+// will result in the metric:
+//
+// `probe_failure_info{reason="problem", label1="value1", label2="value2"}`
+//
+// The corresponding gauge can be obtained with the ProbeResult.failureInfoGauge()
+// method.
+func ProbeFailure(reason string, details ...string) ProbeResult {
+	if len(details)%2 != 0 {
+		panic("Must be called with an odd number of string arguments.")
+	}
+
+	return ProbeResult{success: false, failureReason: reason, failureDetails: details}
+}
+
+func ProbeSuccess() ProbeResult {
+	return ProbeResult{success: true, failureReason: "", failureDetails: nil}
+}
+
+func (r *ProbeResult) failureInfoGauge() *prometheus.GaugeVec {
+	labels := append([]string{"reason", r.failureReason}, r.failureDetails...)
+	return prometheus.NewGaugeVec(probeFailureInfo, labels)
+}
+
+func (r *ProbeResult) log(logger *slog.Logger, duration float64) {
+	if r.success {
+		logger.Info("Probe succeeded", "duration_seconds", duration)
+	} else {
+		// converting the []string slice to an []any slice is a bit finicky
+		logDetails := make([]any, 0, len(r.failureDetails)+2)
+		for _, d := range r.failureDetails {
+			logDetails = append(logDetails, d)
+		}
+		logger.Error(r.failureReason, logDetails...)
+		logger.Error("Probe failed", "duration_seconds", duration)
+	}
+}
+
+type ProbeFn func(ctx context.Context, target string, config config.Module, registry *prometheus.Registry, logger *slog.Logger) ProbeResult
 
 const (
 	helpSSLEarliestCertExpiry     = "Returns last SSL chain expiry in unixtime"
 	helpSSLChainExpiryInTimeStamp = "Returns last SSL chain expiry in timestamp"
 	helpProbeTLSInfo              = "Returns the TLS version used or NaN when unknown"
 	helpProbeTLSCipher            = "Returns the TLS cipher negotiated during handshake"
+	helpProbeFailureInfo          = "Return the reason a Probe failed"
 )
 
 var (
@@ -50,5 +100,10 @@ var (
 	probeTLSCipherGaugeOpts = prometheus.GaugeOpts{
 		Name: "probe_tls_cipher_info",
 		Help: helpProbeTLSCipher,
+	}
+
+	probeFailureInfo = prometheus.GaugeOpts{
+		Name: "probe_failure_info",
+		Help: helpProbeFailureInfo,
 	}
 )
